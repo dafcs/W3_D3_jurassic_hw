62424cd72db66bc76e5744bc1167134b01fd2e92
Daniel FCS <danielf.cs@outlook.com>
2023-05-11 10:59:27 +0100

    last commit, solution

--- models/park.js
+++ models/park.js
@@ -8,13 +8,16 @@ Park.prototype.addDino = function(dino) {
   this.dinosaurs.push(dino)
 };
 
-Park.prototype.removeDino = function(dino_remove) {
-  index_of_dino = 0
-  for (dino of this.dinosaurs) {
-    if (dino.name === dino_remove)
-      index_of_dino = indexOf(dino)
-  }
-  this.dinosaurs.splice(index_of_dino,1)
+Park.prototype.removeDino = function(dinosaur) {
+  // index_of_dino = 0
+  // for (dino of this.dinosaurs) {
+  //   if (dino === dino_remove)
+  //     index_of_dino = indexOf(dino)
+  // }
+  // this.dinosaurs.splice(index_of_dino,1)
+  const index = this.dinosaurs.indexOf(dinosaur);
+  if (index == -1) return
+  this.dinosaurs.splice(index, 1);
 };
 
 Park.prototype.findMostAttrated = function() {
@@ -45,6 +48,30 @@ Park.prototype.calculateVisitors = function() {
   return total
 }
 
+Park.prototype.removeDinosOfSpecie = function(species) {
+  let new_list = []
+  for (dino of this.dinosaurs){
+    if (dino.species !== species){
+      new_list.push(dino)
+    }
+  }
+  this.dinosaurs = new_list
+}
 
+Park.prototype.getDinosPerDiet = function () {
+  const numberOfDinosaursByDiet = {};
+
+  for (const dinosaur of this.dinosaurs) {
+    if (numberOfDinosaursByDiet[dinosaur.diet]) {
+      numberOfDinosaursByDiet[dinosaur.diet] += 1;
+    }
+    else {
+      numberOfDinosaursByDiet[dinosaur.diet] = 1;
+    }
+  }
+
+  return numberOfDinosaursByDiet;
+}
 
 module.exports = Park;
+
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -27,9 +27,10 @@ describe('Park', function() {
   });
 
   it('should be able to add a dinosaur to its collection',function(){
-    park.addDino('Stegossaurus')
-    actual = park.dinosaurs.length
-    assert.strictEqual(actual,1)
+    stego = new Dinosaur('Stegossaurus', 'herbivore',40)
+    park.addDino(stego)
+    actual = park.dinosaurs
+    assert.deepStrictEqual(actual,[stego])
   });
 
   it('should be able to remove a dinosaur from its collection', function() {
@@ -40,8 +41,8 @@ describe('Park', function() {
     park.addDino(stego)
     park.addDino(rapto)
     park.removeDino(stego)
-    actual = park.dinosaurs.length
-    assert.strictEqual(actual,2)
+    actual = park.dinosaurs
+    assert.deepStrictEqual(actual,[trex,rapto])
   });
 
   it('should be able to find the dinosaur that attracts the most visitors', function() {
@@ -52,7 +53,7 @@ describe('Park', function() {
     park.addDino(stego)
     park.addDino(rapto)
     actual = park.findMostAttrated()
-    assert.strictEqual(actual.species,'raptor')
+    assert.strictEqual(actual,rapto)
   });
 
   it('should be able to find all dinosaurs of a particular species', function() {
@@ -65,7 +66,7 @@ describe('Park', function() {
     park.addDino(rapto)
     park.addDino(raptok)
     dino_list = park.findBySpecies('raptor')
-    assert.strictEqual(dino_list.length,2)
+    assert.deepStrictEqual(dino_list,[rapto,raptok])
   });
 
   it('should be able to calculate the total number of visitors per day', function() {
@@ -106,4 +107,38 @@ describe('Park', function() {
   actual = (park.ticket_price * park.calculateVisitors())*365
   assert.strictEqual(actual,3436475)  
   });
+
+  it('should be able to remove all dinosaurs of a particular species', function (){
+    trex = new Dinosaur('t-rex', 'carnivore', 50)
+    stego = new Dinosaur('stegossaurus', 'herbivore',40)
+    rapto = new Dinosaur('raptor', 'carnivore',90)
+    raptok = new Dinosaur('raptor', 'carnivore',89)
+    park.addDino(trex)
+    park.addDino(stego)
+    park.addDino(rapto)
+    park.addDino(raptok) 
+    park.removeDinosOfSpecie('raptor')
+    actual = park.dinosaurs.length
+    assert.deepStrictEqual(actual,2)
+  })
+
+  it('should be able to show number of dinos per diet', function (){
+    trex = new Dinosaur('t-rex', 'carnivore', 50)
+    stego = new Dinosaur('stegossaurus', 'herbivore',40)
+    rapto = new Dinosaur('raptor', 'carnivore',90)
+    raptok = new Dinosaur('raptor', 'carnivore',89)
+    park.addDino(trex)
+    park.addDino(stego)
+    park.addDino(rapto)
+    park.addDino(raptok)
+    compare = park.getDinosPerDiet()
+    actual = {'carnivore':3,'herbivore':1}
+    assert.deepStrictEqual(actual,compare)
+  })
+
+
 });
+
+
+
+
